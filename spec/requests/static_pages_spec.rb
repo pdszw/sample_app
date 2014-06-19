1ef596976eaf219d10fcd94013bae9f0bcc85134
pdszw <peter.szweryn@gmail.com>
2014-06-19 19:51:35 +1000

    add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,28 @@
 require 'spec_helper'
 
 describe "Static pages" do
+    
+    let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
     describe "Home page" do
         it "should have the content 'Sample App'" do
             visit '/static_pages/home'
             expect(page).to have_content('Sample App')
         end
 
-        it "should have the title 'Home'" do
+        it "should have the base title" do
             visit '/static_pages/home'
-            expect(page).to have_title('Home')
+            expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+        end
+
+#        it "should have the title 'Home'" do
+#            visit '/static_pages/home'
+#            expect(page).to have_title("#{base_title} | Home")
+#        end
+
+        it "should not have a custom page title" do
+            visit '/static_pages/home'
+            expect(page).not_to have_title('| Home')
         end
     end
 
@@ -21,7 +34,7 @@ describe "Static pages" do
 
         it "should have the title 'Help'" do
             visit '/static_pages/help'
-            expect(page).to have_title('Help')
+            expect(page).to have_title("#{base_title} | Help")
         end
     end
 
@@ -33,7 +46,19 @@ describe "Static pages" do
 
         it "should have the title 'About Us'" do
             visit '/static_pages/about'
-            expect(page).to have_title('About Us')
+            expect(page).to have_title("#{base_title} | About Us")
+        end
+    end
+
+    describe "Contact page" do
+        it "should have the content 'Contact Us'" do
+            visit '/static_pages/contact'
+            expect(page).to have_content('Contact Us')
+        end
+        
+        it "should have the title 'Contact Us'" do
+            visit '/static_pages/contact'
+            expect(page).to have_title("#{base_title} | Contact Us")
         end
     end
 end
