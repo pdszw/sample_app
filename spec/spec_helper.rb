8036f71c5158d2f911db50db2d2665362dbfdd55
pdszw <peter.szweryn@gmail.com>
2014-06-24 21:33:49 +1000

    finish layout and routes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@ Spork.prefork do
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in ./spec/support/ and its sub-directories.
-  Dir[Rails.root.join("spec/suppot/**/*.rb")].each {|f| require f}
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
   # Checks for pending migrations before tests are run.
   # If you are not using ActiveRecord, you can remove this line
